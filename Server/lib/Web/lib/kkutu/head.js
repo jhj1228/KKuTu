199900c785706acc95b62d430b26dd280f50c0fc
jhj1228 <kbg5113@gmail.com>
2025-12-30 19:43:25 +0900

    1.0.0 ~ 1.0.3

---
+++ Server/lib/Web/lib/kkutu/head.js
@@ -0,0 +1,231 @@
+/**
+ * Rule the words! KKuTu Online
+ * Copyright (C) 2017 JJoriping(op@jjo.kr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var MODE;
+var BEAT = [ null,
+	"10000000",
+	"10001000",
+	"10010010",
+	"10011010",
+	"11011010",
+	"11011110",
+	"11011111",
+	"11111111"
+];
+var NULL_USER = {
+	profile: { title: L['null'] },
+	data: { score: 0 }
+};
+var MOREMI_PART;
+var AVAIL_EQUIP;
+var RULE;
+var OPTIONS;
+var MAX_LEVEL = 360;
+var TICK = 30;
+var EXP = [];
+var BAD = new RegExp([ "느으*[^가-힣]*금마?", "니[^가-힣]*(엄|앰|엠)", "(ㅄ|ㅅㅂ|ㅂㅅ)", "미친(년|놈)?", "(병|븅|빙)[^가-힣]*신", "보[^가-힣]*지", "(새|섀|쌔|썌)[^가-힣]*(기|끼)", "섹[^가-힣]*스", "(시|씨|쉬|쒸)이*입?[^가-힣]*(발|빨|벌|뻘|팔|펄)", "십[^가-힣]*새", "씹", "(애|에)[^가-힣]*미", "자[^가-힣]*지", "존[^가-힣]*나", "좆|죶", "지랄", "창[^가-힣]*(녀|년|놈)", "fuck", "sex" ].join('|'), "g");
+
+var ws, rws;
+var $stage;
+$audiosets = [
+	[
+		{
+			key: "lobby",
+			value: "/media/kkutu/LobbyBGM.mp3"
+		},
+		{
+			key: "lobbyseol",
+			value: "/media/kkutu/LobbySeolBGM.mp3"
+		},
+		{
+			key: "lobbuoriginal",
+			value: "/media/kkutu/LobbyBGM.mp3"
+		},
+		{
+			key: "lobbyending",
+			value: "/media/kkutu/LobbyBGMending.mp3"
+		},
+		{
+			key: "inthepool",
+			value: "/media/kkutu/LobbyINTHEPOOL.mp3"
+		},
+		{
+			key: "enchanted",
+			valye: "/media/kkutu/LobbyEnchantedlove.mp3"
+		},
+		{
+			key: "k",
+			value: "/media/kkutu/k.mp3"
+		},
+		{
+			key: "jaqwi",
+			value: "/media/kkutu/JaqwiBGM.mp3"
+		},
+		{
+			key: "jaqwiF",
+			value: "/media/kkutu/JaqwiFastBGM.mp3"
+		},
+		{
+			key: "game_start",
+			value: "/media/kkutu/game_start.mp3"
+		},
+		{
+			key: "round_start",
+			value: "/media/kkutu/round_start.mp3"
+		},
+		{
+			key: "fail",
+			value: "/media/kkutu/fail.mp3"
+		},
+		{
+			key: "lvup",
+			value: "/media/kkutu/lvup.mp3"
+		},
+		{
+			key: "Al",
+			value: "/media/kkutu/Al.mp3"
+		},
+		{
+			key: "success",
+			value: "/media/kkutu/success.mp3"
+		},
+		{
+			key: "missing",
+			value: "/media/kkutu/missing.mp3"
+		},
+		{
+			key: "mission",
+			value: "/media/kkutu/mission.mp3"
+		},
+		{
+			key: "kung",
+			value: "/media/kkutu/kung.mp3"
+		},
+		{
+			key: "horr",
+			value: "/media/kkutu/horr.mp3"
+		},
+		{
+			key: "timeout",
+			value: "/media/kkutu/timeout.mp3"
+		}
+	],
+	[
+		{
+			key: "lobby",
+			value: "/media/kkutu/LobbyBGM.mp3"
+		},
+		{
+			key: "lobbyseol",
+			value: "/media/kkutu/LobbySeolBGM.mp3"
+		},
+		{
+			key: "lobbyending",
+			value: "/media/kkutu/LobbyBGMending.mp3"
+		},
+		{
+			key: "inthepool",
+			value: "/media//kkutu/LobbyINTHEPOOL.mp3"
+		},
+		{
+			key: "enchanted",
+			valye: "/media/kkutu/LobbyEnchantedlove.mp3"
+		},
+		{
+			key: "k",
+			value: "/media/kkutu/k.mp3"
+		},
+		{
+			key: "jaqwi",
+			value: "/media/kkutu/JaqwiBGM.mp3"
+		},
+		{
+			key: "jaqwiF",
+			value: "/media/kkutu/JaqwiFastBGM.mp3"
+		},
+		{
+			key: "game_start",
+			value: "/media/kkutu/game_start.mp3"
+		},
+		{
+			key: "round_start",
+			value: "/media/kkutu/round_start.mp3"
+		},
+		{
+			key: "fail",
+			value: "/media/kkutu/fail.mp3"
+		},
+		{
+			key: "lvup",
+			value: "/media/kkutu/lvup.mp3"
+		},
+		{
+			key: "Al",
+			value: "/media/kkutu/Al.mp3"
+		},
+		{
+			key: "success",
+			value: "/media/kkutu/success.mp3"
+		},
+		{
+			key: "missing",
+			value: "/media/kkutu/missing.mp3"
+		},
+		{
+			key: "mission",
+			value: "/media/kkutu/mission.mp3"
+		},
+		{
+			key: "kung",
+			value: "/media/kkutu/kung.mp3"
+		},
+		{
+			key: "horr",
+			value: "/media/kkutu/horr.mp3"
+		},
+		{
+			key: "timeout",
+			value: "/media/mahankkutu/timeout.mp3"
+		}
+	]
+];
+for (i = 0; i <= 10; i++) $audiosets[0].push(
+	{
+		key: "T" + i,
+		value: "/media/kkutu/T" + i + ".mp3"
+	},
+	{
+		key: "K" + i,
+		value: "/media/kkutu/K" + i + ".mp3"
+	},
+	{
+		key: "As" + i,
+		value: "/media/kkutu/As" + i + ".mp3"
+	}
+)
+var $sound = {};
+var $_sound = {}; // 현재 재생 중인 것들
+var $data = {};
+var $lib = { Classic: {}, Jaqwi: {}, Crossword: {}, Typing: {}, Hunmin: {}, Daneo: {}, Sock: {}, Drawing: {}, Moqwi: {}, All: {} };
+var $rec;
+var mobile;
+
+var audioContext = window.hasOwnProperty("AudioContext") ? (new AudioContext()) : false;
+var _WebSocket = window['WebSocket'];
+var _setInterval = setInterval;
+var _setTimeout = setTimeout;
---
+++ Server/lib/const.js
@@ -0,0 +1,293 @@
+/**
+ * Rule the words! KKuTu Online
+ * Copyright (C) 2017 JJoriping(op@jjo.kr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var GLOBAL = require("./sub/global.json");
+
+exports.KKUTU_MAX = 50;
+exports.MAIN_PORTS = GLOBAL.MAIN_PORTS;
+exports.TEST_PORT = 4040;
+exports.SPAM_CLEAR_DELAY = 1600;
+exports.SPAM_ADD_DELAY = 750;
+exports.SPAM_LIMIT = 7;
+exports.BLOCKED_LENGTH = 10000;
+exports.KICK_BY_SPAM = 9;
+exports.MAX_OBSERVER = 4;
+exports.TESTER = GLOBAL.ADMIN.concat([
+	"Input tester id here"
+]);
+exports.IS_SECURED = GLOBAL.IS_SECURED;
+exports.SSL_OPTIONS = GLOBAL.SSL_OPTIONS;
+exports.OPTIONS = {
+	'man': { name: "Manner" },
+	'ext': { name: "Injeong" },
+	'mis': { name: "Mission" },
+	'loa': { name: "Loanword" },
+	'prv': { name: "Proverb" },
+	'str': { name: "Strict" },
+	'k32': { name: "Sami" },
+	'no2': { name: "No2" },
+	'ulm': { name: "Unlimited" },
+	'sht': { name: "Short" },
+	'rdt': { name: "Randomturn" }
+};
+exports.MOREMI_PART = [ 'back', 'eye', 'mouth', 'shoes', 'clothes', 'head', 'lhand', 'rhand', 'front' ];
+exports.CATEGORIES = [ "all", "spec", "skin", "badge", "head", "eye", "mouth", "clothes", "hs", "back" ];
+exports.AVAIL_EQUIP = [
+	"NIK", "BDG1", "BDG2", "BDG3", "BDG4",
+	"Mhead", "Meye", "Mmouth", "Mhand", "Mclothes", "Mshoes", "Mback"
+];
+exports.GROUPS = {
+	'spec': [ "PIX", "PIY", "PIZ", "CNS" ],
+	'skin': [ "NIK" ],
+	'badge': [ "BDG1", "BDG2", "BDG3", "BDG4" ],
+	'head': [ "Mhead" ],
+	'eye': [ "Meye" ],
+	'mouth': [ "Mmouth" ],
+	'clothes': [ "Mclothes" ],
+	'hs': [ "Mhand", "Mshoes" ],
+	'back': [ "Mback", "Mfront" ]
+};
+exports.RULE = {
+/*
+	유형: { lang: 언어,
+		rule: 이름,
+		opts: [ 추가 규칙 ],
+		time: 시간 상수,
+		ai: AI 가능?,
+		big: 큰 화면?,
+		ewq: 현재 턴 나가면 라운드 종료?
+	}
+*/
+	'KKT': { lang: "ko",
+		rule: "Classic",
+		opts: [ "man", "ext", "mis", "loa", "str", "k32", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: true
+	},
+	'KSH': { lang: "ko",
+		rule: "Classic",
+		opts: [ "man", "ext", "mis", "loa", "str", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: true
+	},
+	'CSQ': { lang: "ko",
+		rule: "Jaqwi",
+		opts: [ "ijp" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: false
+	},
+	'KCW': { lang: "ko",
+		rule: "Crossword",
+		opts: [],
+		time: 2,
+		ai: false,
+		big: true,
+		ewq: false
+	},
+	'KTY': { lang: "ko",
+		rule: "Typing",
+		opts: [ "prv" ],
+		time: 1,
+		ai: false,
+		big: false,
+		ewq: false
+	},
+	'KAP': { lang: "ko",
+		rule: "Classic",
+		opts: [ "man", "ext", "mis", "loa", "str", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		_back: true,
+		ewq: true
+	},
+	'HUN': { lang: "ko",
+		rule: "Hunmin",
+		opts: [ "ext", "mis", "loa", "str", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: true
+	},
+	'KDA': { lang: "ko",
+		rule: "Daneo",
+		opts: [ "ijp", "mis", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: true
+	},
+	'KSS': { lang: "ko",
+		rule: "Sock",
+		opts: [ "no2" ],
+		time: 1,
+		ai: false,
+		big: true,
+		ewq: false
+	},
+	'KMH': { lang: "ko",
+		rule: "Classic",
+		opts: [ "man", "ext", "mis", "loa", "str", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: false
+	},
+	'KKK': { lang: "ko",
+		rule: "Classic",
+		opts: [ "man", "ext", "mis", "loa", "str", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: false
+	},
+	'KDG': { lang: "ko",
+		rule: "Drawing",
+		opts: [ "ijp", "sht", "ulm" ],
+		time: 1,
+		ai: false,
+		big: true,
+		ewq: true
+	},
+	'MOQ': { lang: "ko",
+		rule: "Moqwi",
+		opts: [ "ijp" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: false
+	},
+	'ALL': { lang: "ko",
+		rule: "All",
+		opts: [ "mis", "rdt" ],
+		time: 1,
+		ai: true,
+		big: false,
+		ewq: true
+	},
+	'KTT': { lang: "ko",
+		rule: "Typing",
+		opts: [ "ijp" ],
+		time: 1,
+		ai: false,
+		big: false,
+		ewq: false
+	}
+};
+exports.getPreScore = function(text, chain, tr){
+	return 2 * (Math.pow(5 + 7 * (text || "").length, 0.74) + 0.88 * (chain || []).length) * ( 0.5 + 0.5 * tr ) * 3.5;
+};
+exports.getPenalty = function(chain, score){
+	return -1 * Math.round(Math.min(10 + (chain || []).length * 2.1 + score * 0.15, score));
+};
+exports.GAME_TYPE = Object.keys(exports.RULE);
+exports.EXAMPLE_TITLE = {
+	'ko': "가나다라마바사아자차",
+	'en': "abcdefghij"
+};
+exports.INIT_SOUNDS = [ "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "○" ];
+exports.INIT_VOWELS = [ "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ" ];
+exports.MISSION_ko = [ "가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하" ];
+exports.MISSION_en = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+
+exports.KO_INJEONG = [
+	"IMS", // THE iDDOLM@STER
+	"VOC", // VOCALOID
+	"RAG", // 간식
+	"NEX", // 게임
+	"KTV", // 국내 방송 프로그램
+	"CPY", // 기업
+	"NFX", // 넷플릭스
+	"KOT", // 대한민국 철도역
+	"DOT", // 도타 2
+	"THP", // 동방 프로젝트
+	"JLN", // 라이트 노벨
+	"LVL", // 러브 라이브!
+	"LOL", // 리그 오브 레전드
+	"JAN", // 만화/애니메이션
+	"MAP", // 메이플스토리
+	"BGP", // 뱅드림! 걸즈 밴드 파티!
+	"BLA", // 블루 아카이브
+	"CYP", // 사이퍼즈 
+	"STA", // 스타크래프트
+	"ESB", // 앙상블 스타즈!
+	"KMV", // 영화
+	"ELW", // 엘소드
+	"OVW", // 오버워치
+	"WOW", // 월드 오브 워크래프트
+	"WEB", // 웹툰
+	"KPO", // 유명인
+	"JPT", // 일본 철도역
+	"TCG", // 트릭컬 리바이브
+	"POK", // 포켓몬스터
+	"HSS", // 하스스톤
+	"HAK", // 학교
+	"KMU", // 한국 대중음악
+	"KRP", // 한국 라디오 프로그램
+	"HOS" // 히어로즈 오브 더 스톰
+];
+exports.EN_INJEONG = [
+	"LOL"
+];
+exports.KO_THEME = [
+	"30", "40", "60", "80", "90",
+	"140", "150", "160", "170", "190",
+	"220", "230", "240", "270", "310",
+	"320", "350", "360", "420", "430",
+	"450", "490", "530"
+];
+exports.EN_THEME = [
+	"e05", "e08", "e12", "e13", "e15",
+	"e18", "e20", "e43"
+];
+exports.IJP_EXCEPT = [
+	"OIJ"
+];
+exports.KO_IJP = exports.KO_INJEONG.concat(exports.KO_THEME).filter(function(item){ return !exports.IJP_EXCEPT.includes(item); });
+exports.EN_IJP = exports.EN_INJEONG.concat(exports.EN_THEME).filter(function(item){ return !exports.IJP_EXCEPT.includes(item); });
+exports.REGION = {
+	'en': "en",
+	'ko': "kr"
+};
+exports.KOR_STRICT = /(^|,)(1|INJEONG)($|,)/;
+exports.KOR_GROUP = new RegExp("(,|^)(" + [
+	"0", "1", "3", "7", "8", "11", "9",
+	"16", "15", "17", "2", "18", "20", "26", "19",
+	"INJEONG"
+].join('|') + ")(,|$)");
+exports.ENG_ID = /^[a-z]+$/i;
+exports.KOR_FLAG = {
+	LOANWORD: 1, // 외래어
+	INJEONG: 2,	// 어인정
+	SPACED: 4, // 띄어쓰기를 해야 하는 어휘
+	SATURI: 8, // 방언
+	OLD: 16, // 옛말
+	MUNHWA: 32 // 문화어
+};
+exports.WP_REWARD = function(){
+	return 10 + Math.floor(Math.random() * 91);
+};
+exports.getRule = function(mode){
+	return exports.RULE[exports.GAME_TYPE[mode]];
+};
