09520feddc2805623ddf89c279bbb9275e6c391d
jhj1228 <kbg5113@gmail.com>
2026-01-02 00:15:20 +0900

    Mistake

--- Server/lib/Game/games/hunmin.js
+++ Server/lib/Game/games/hunmin.js
@@ -51,7 +51,7 @@ exports.roundReady = function () {
 	if (my.game.round <= my.round) {
 		my.game.theme = getTheme(2, my.game.done);
 		my.game.chain = [];
-		if (my.opts.mission) my.game.mission = getMission(my.rule.lang);
+		if (my.opts.mission) my.game.mission = getMission(my.game.theme);
 		my.game.done.push(my.game.theme);
 		my.byMaster('roundReady', {
 			round: my.game.round,
@@ -168,7 +168,7 @@ exports.submit = function (client, text, data) {
 						bonus: (my.game.mission === true) ? score - my.getScore(text, t, true) : 0
 					}, true);
 					if (my.game.mission === true || (my.opts.mission && my.opts.randommission)) {
-						my.game.mission = getMission(my.rule.lang);
+						my.game.mission = getMission(my.game.theme);
 					}
 					setTimeout(my.turnNext, my.game.turnTime / 6);
 					if (!client.robot) {
