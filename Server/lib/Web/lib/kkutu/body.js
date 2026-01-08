402ed5129a455c98e7520695c966ea5f9839fe2c
jhj1228 <kbg5113@gmail.com>
2026-01-08 22:23:50 +0900

    Update Media Files

--- Server/lib/Web/lib/kkutu/body.js
+++ Server/lib/Web/lib/kkutu/body.js
@@ -2710,7 +2710,7 @@ function playSound(key, loop) {
 	if ($_sound[key]) $_sound[key].stop();
 	$_sound[key] = src;
 	src.originalKey = key;
-	src.key = key === "lobby" || key === "lobbyoriginal" || key === "lobbyseol" || key === "lobbyending" || key === "lobbymuseum" || key === "inthepool" || key === "enchanted" || key === "scarletzone" ? "lobby" : key;
+	src.key = key === "lobby" || key === "lobbyseol" || key === "inthepool" || key === "enchanted" ? "lobby" : key;
 
 	src.start();
 
